7f725683af401ea1f8fbde4fdce2e9f4f0c73173
ItZRockyboi <112095621+ItZRockyboi@users.noreply.github.com>
2023-02-09 15:55:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+canvas = createCanvas(640,480);
+canvas.position(110,250);
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+function draw(){
+image(video,0,0,640,480);
+
+
+}
+
+function preload(){
+
+}
+
+function take_snapshot(){
+    save("picture_1.png");
+}
+
+
